a74c053a704869f00de8051c9f644bca4a361a95
MrShifu01 <stander.christian.com>
2023-07-20 08:53:34 +0200

    a

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,22 +1,25 @@
-import '../App.css';
+import React from 'react';
 
 const Hero = () => {
   return (
-    <div className='container-fluid hero-image vh-100' id='hero'>
-        <div className='row'>
-          <div className='col-md-8 offset-md-3'>
-            <div className='d-flex flex-column hero-text'>
-              <h6 className='text-uppercase text-sm'>Welcome to</h6>
-              <h1 className='heading'>Matumi Corporate Advisers</h1>
-              <p className='lead'>Matumi is a boutique M&A advisory firm specializing in end-to-end merger and acquisition services across various sectors, including healthcare, logistics, and technology.</p>
-              <div>
-                <button className='btn btn-lg btn-light mt-2' variant="light" size="lg">
-                  Learn More
-                </button>
-              </div>
-            </div>
-          </div>
+    <div className='hero-container'>
+      {/* Container for the background image */}
+      <div className='hero-image'></div>
+      <div className='hero-overlay'></div>
+
+      {/* Container for the hero text */}
+      <div className='hero-text'>
+        <h6 className='text-uppercase text-sm'>Welcome to</h6>
+        <h1 className='heading'>Matumi Corporate Advisers</h1>
+        <p className='lead'>
+          Matumi is a boutique M&A advisory firm specializing in end-to-end merger and acquisition services across various sectors, including healthcare, logistics, and technology.
+        </p>
+        <div>
+          <button className='btn btn-lg btn-light mt-2' variant="light" size="lg">
+            Learn More
+          </button>
         </div>
+      </div>
     </div>
   );
 };
---
+++ frontend/src/components/Transactions.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Transactions = () => {
+  return (
+    <section className='transactions bg-secondary' id='transactions'>
+        Testing
+    </section>
+  )
+}
+
+export default Transactions
